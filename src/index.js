077f2173a8dfee21a8bb74a8ce3ac8020d23c1d8
Liliya Smelova <26244983+LiliyaSm@users.noreply.github.com>
2020-09-29 14:02:55 +0300

    feat: solution passes all tests

--- src/index.js
+++ src/index.js
@@ -8,46 +8,45 @@ module.exports = function check(str, bracketsConfig) {
         openBracket.push(v[0]);
         closeBracket.push(v[1]);
     });
-    console.log("openBracket", openBracket);
-    console.log("closeBracket", closeBracket);
 
+    //main idea - if bracket from array is closing we check bracket before it in stack and pop it, opening bracket just push in stack
     for (let i = 0; i < arr.length; i++) {
-
-        if (stack.length > 0 && arr[i] === "|") {
+        // case : open bracket and close are the same
+        if (
+            stack.length > 0 &&
+            openBracket.includes(arr[i]) &&
+            closeBracket.includes(arr[i])
+        ) {
             let lastBracket = stack[stack.length - 1];
-            if (lastBracket === "|") {
-                stack.pop()
+
+            if (lastBracket === arr[i]) {
+                // bracket from array is closing bracket, pop bracket from stack
+                stack.pop();
                 continue;
             } else {
-            stack.push(arr[i]);
-            continue;
+                // bracket from array is opening bracket
+                stack.push(arr[i]);
+                continue;
             }
         }
 
-
         if (openBracket.includes(arr[i])) {
             stack.push(arr[i]);
-            console.log("stack", stack);
+
             continue;
         }
 
-
         if (stack.length === 0) {
+            // case - bracket is closing and array is empty
             return false;
         }
 
-        console.log("arr[i]", arr[i]);
-        
+        //bracket from array is closing, compare with pair, index for both is the same
         let index = openBracket.indexOf(stack.pop());
-        console.log(index);
         if (closeBracket[index] === arr[i]) {
-            console.log("closeBracket[index]", closeBracket[index]);
-            console.log("arr[i]", arr[i]);
             continue;
         } else {
-            console.log(" return false", "retfalse");
-            console.log("closeBracket[index]", closeBracket[index]);
-            console.log("arr[i]", arr[i]);
+            // error here
             return false;
         }
     }
